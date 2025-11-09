fd7f37110740e9cfac7f0fdf185cdfcba81fc9f6
jonlai211 <2946443950@qq.com>
2025-11-08 22:02:07 -0500

    add: database by sqlite3

--- server/app.py
+++ server/app.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import logging
-
-from routes.product import bp as product_bp
-
-app = Flask(__name__)
-CORS(app)
+import os
 
 # Basic logging configuration for development
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s %(message)s")
 
+from routes.product import bp as product_bp
+from database import init_db
+
+app = Flask(__name__)
+CORS(app)
 
 # Register blueprints
 app.register_blueprint(product_bp)
@@ -24,4 +25,6 @@ def home():
 
 
 if __name__ == '__main__':
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+            init_db()
     app.run(debug=True, host='0.0.0.0', port=5000)
